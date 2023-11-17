da3ed654a789c2c3b523d49916bd1576b5f0e6bf
mcalisadeghi <ali.ke970@gmail.com>
2023-11-17 13:08:42 +0330

    add widget MyBox &MyButton to homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/components/box.dart';
+import 'package:flutter_application_1/components/button.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({
@@ -7,6 +9,17 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[500],
+      body: Center(
+        child: MyBox(
+          color: Colors.deepPurple[300],
+          child: MyButton(
+            color: Colors.deepPurple[200],
+            onTap: () {},
+          ),
+        ),
+      ),
+    );
   }
 }

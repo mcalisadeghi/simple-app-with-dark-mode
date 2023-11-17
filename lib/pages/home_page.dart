8e57bfbdce533f58dc04dc9c0f0ea56655052c7f
mcalisadeghi <ali.ke970@gmail.com>
2023-11-17 13:05:31 +0330

    add GestureDetector and ... in bUtton

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
